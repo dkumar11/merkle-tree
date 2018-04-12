828745c7e02f2d47355e2bd269cc3bb8a79a7ddd
Divyashish kumar <dkumar11@berkeley.edu>
2018-04-12 15:45:10 -0700

    Finally complete merkle tree

--- merkle_proof.py
+++ merkle_proof.py
@@ -8,10 +8,25 @@ def merkle_proof(tx, merkle_tree):
     parse through it to arrive at the minimum amount of information required
     to arrive at the correct block header. This does not include the tx
     itself.
-
     Return this data as a list; remember that order matters!
     """
-    #### YOUR CODE HERE
+    return merkle_proof_helper(merkle_tree.leaves, tx, [])
+
+def merkle_proof_helper(txs, tx, nodes):
+    if len(txs) > 1:
+        lst = []
+        index = 0
+        for idx in range(0, len(txs), 2):
+            lst.append(hash_data( txs[idx + 1]  + txs[idx]))
+            if (txs[idx + 1] == tx):
+                nodes.insert(0, Node('l', txs[idx]))
+                index = hash_data(txs[idx] + tx)
+            elif (txs[idx] == tx):
+                nodes.insert(0, Node('r', txs[idx + 1]))
+                index = hash_data(tx + txs[idx + 1])
+        return merkle_proof_helper(lst, index, nodes)
+    else:
+        return nodes
 
 
 
@@ -20,7 +35,13 @@ def verify_proof(tx, merkle_proof):
     that the correct block header can be retrieved by properly hashing the tx
     along with every other piece of data in the proof in the correct order
     """
-    #### YOUR CODE HERE
 
-
-    
+    return_value = tx
+    for data_value in merkle_proof[::-1]:
+        if data_value.direction == 'r':
+            return_value = hash_data(return_value + data_value.tx)
+        elif data_value.direction == 'l':
+            return_value = hash_data(data_value.tx + return_value)
+        else:
+            continue
+    return return_value
